0ac6860c168e0ea0bc3a76e73a9324856d0cc076
daazzzy <damian71253@gmail.com>
2024-04-17 17:46:54 +0300

    nvim

---
+++ nvima/lua/chadrc.lua
@@ -0,0 +1,13 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+	theme = "chocolate",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+M.plugins = "plugins"
+return M 
